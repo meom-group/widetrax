6be9e1fbc3a436c92f3dc8e27bd44cfc0436f8d6
Amine-ouhechou <ouhechou.amine@gmail.com>
2024-07-08 22:14:40 +0200

    new functions

--- widetrax.py
+++ widetrax.py
@@ -7,6 +7,19 @@ import pyinterp
 import pyinterp.fill as fill
 import scipy.signal as signal
 
+import re
+from datetime import datetime
+
+from collections import defaultdict
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import cartopy.feature as cfeature
+
+# =============================================================================
+# extract_xarray_in_region
+# =============================================================================
+
 
 def extract_xarray_in_region(directory, area):
     """
@@ -57,7 +70,11 @@ def extract_xarray_in_region(directory, area):
 
     return datasets
 
-####
+
+# =============================================================================
+# count_observations
+# =============================================================================
+
 
 def count_observations(datasets, area, resolution):
     """
@@ -103,7 +120,11 @@ def count_observations(datasets, area, resolution):
     
     return obs_count
 
-####
+
+# =============================================================================
+# fill_nan
+# =============================================================================
+
 
 def fill_nan(datasets):
    
@@ -151,12 +172,15 @@ def fill_nan(datasets):
         
     return has_converged, filled_datasets
 
-####
+
+# =============================================================================
+# retrieve_segments
+# =============================================================================
 
 
 # to be included in the function retrive_segments
-for key in range(len(filled_datasets)):
-    filled_datasets[key]['ssh'] = filled_datasets[key]['ssha'] + filled_datasets[key]['mdt']
+#for key in range(len(filled_datasets)):
+#    filled_datasets[key]['ssh'] = filled_datasets[key]['ssha'] + filled_datasets[key]['mdt']
     
 def retrieve_segments(datasets):
     """
@@ -168,12 +192,19 @@ def retrieve_segments(datasets):
     """
     segments_dict = {}
     counter = 0
-
+    
+    #Calcul de SSH = ssha + mdt
+    for ky in range(len(datasets)):
+        datasets[ky]['ssh'] = datasets[ky]['ssha'] + datasets[ky]['mdt']
+    
     for key, dataset in datasets.items():
         for col in range(dataset.dims['num_pixels']):
             # Extract data for one column
             col_data = dataset.isel(num_pixels=col)
-                       
+            
+            # Delete coords to avoid duplicates and variables we don't need
+            col_dataset = col_data.drop_vars(['latitude', 'longitude','ssha', 'mdt'])
+            
             # Check whether the column is entirely NaN
             if not np.all(np.isnan(col_dataset['ssh'])):
                 segment_data = col_dataset.to_array().values.squeeze()
@@ -191,7 +222,11 @@ def retrieve_segments(datasets):
                 
     return segments_dict
 
-####
+
+# =============================================================================
+# calculate_psd
+# =============================================================================
+
 
 def calculate_psd(segments_dict):
     """
@@ -216,3 +251,156 @@ def calculate_psd(segments_dict):
             new_key += 1
 
     return psd_dict, freqs_dict
+
+
+# =============================================================================
+# check_directory
+# =============================================================================
+
+
+def check_directory(database_path, start_date_str, end_date_str):
+    # Regex pattern to match folder names like cyc_001, cyc_002, etc.
+    folder_pattern = re.compile(r'cycle_\d{3}')
+    # Regex pattern to match the date in the file name SWOT........_YYYYMMDD...
+    file_pattern = re.compile(r'SWOT.*_(\d{8})T.*\.nc')
+    
+    # Convert date strings to datetime objects
+    start_date = datetime.strptime(start_date_str, '%Y%m%d')
+    end_date = datetime.strptime(end_date_str, '%Y%m%d')
+    
+    # List to store the names of folders that meet the criteria
+    matching_folders = []
+    
+    try:
+        # List all items in the database directory
+        items = os.listdir(database_path)
+        
+        for item in items:
+            folder_path = os.path.join(database_path, item)
+            
+            # Check if the item is a directory and matches the pattern
+            if os.path.isdir(folder_path) and folder_pattern.match(item):
+                netcdf_files = [f for f in os.listdir(folder_path) if f.endswith('.nc')]
+                for nc_file in netcdf_files:
+                    match = file_pattern.search(nc_file)
+                    if match:
+                        file_date_str = match.group(1)
+                        file_date = datetime.strptime(file_date_str, '%Y%m%d')
+                        
+                        if start_date <= file_date <= end_date:
+                            matching_folders.append(item)
+                            break  # Stop checking files in this folder once a match is found
+        
+        return matching_folders
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+    
+# =============================================================================
+# extract_xarrays_by_time
+# =============================================================================
+
+
+def extract_xarrays_by_time(database_path, start_date_str, end_date_str,area):
+    
+    matching_folders = check_directory(database_path, start_date_str, end_date_str)
+    combined_datasets_dict = defaultdict(list)
+    current_key = 0
+    
+    for folder in matching_folders:
+        folder_path = os.path.join(database_path, folder)
+        result_dict = extract_xarray_in_region(folder_path,area)
+    
+        # Add entries to the combined_dict with sequential keys
+        for value in result_dict.values():
+            combined_datasets_dict[current_key] = value
+            current_key += 1
+            
+            
+    # Convert defaultdict back to dict
+    combined_datasets_dict = dict(combined_datasets_dict)
+    
+    return combined_datasets_dict
+
+
+# =============================================================================
+# extract_xarrays_by_time
+# =============================================================================
+
+
+def plot_obs_count(ax, obs_count, area,title=None):
+    """
+    Plots the observation count per bin.
+
+    Parameters:
+    - ax: Matplotlib Axes instance
+    - obs_count: 2D array of observation counts
+    - area: list
+    """
+    lon_min,lat_min,lon_max,lat_max = area
+    
+    #if lon_min > 180:
+    #    lon_min = lon_min - 360
+        
+    #if lon_max > 180:
+    #    lon_max = lon_max - 360
+        
+        
+    ax.add_feature(cfeature.LAND, facecolor='gray')
+    
+    ax.set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
+    
+    # Create a mask for zero values to present the continent in its color
+    obs_count_masked = np.ma.masked_where(obs_count == 0, obs_count)
+    
+    im = ax.imshow(obs_count_masked, extent=[lon_min, lon_max, lat_min, lat_max], 
+                   origin='lower', cmap="jet", transform=ccrs.PlateCarree(), 
+                   vmin=0, vmax=500)
+    
+    
+    ax.coastlines()
+    ax.gridlines(draw_labels=True)
+    
+    plt.colorbar(im, ax=ax, label='Number of observations per bin',shrink=0.5)
+    
+    # Set the title if provided
+    if title:
+        ax.set_title(title, fontsize=15, fontweight='bold', color='black')
+
+        
+# =============================================================================
+# psd_mean_and_freq
+# =============================================================================        
+ 
+    
+def psd_mean_and_freq(psd_dict, freqs_dict):
+    max_length = max(len(array) for array in psd_dict.values())
+
+    # Initialize 2 arrays.
+    psd_sum = np.zeros(max_length)
+    counts = np.zeros(max_length)
+
+    for array in psd_dict.values():
+        # Adjust the array to the maximum size by adding NaNs
+        padded_array = np.full(max_length, np.nan)
+        padded_array[:len(array)] = array
+        
+        # Calculate the sum of the PSD values, ignoring the NaNs
+        psd_sum = np.nansum([psd_sum, padded_array], axis=0)
+        
+        # Count the non-NaN values
+        counts = np.nansum([counts, ~np.isnan(padded_array)], axis=0)
+
+    # Calculate the PSD mean (np.array)
+    psd_mean = psd_sum / counts
+
+    # take the frequency of the longest column
+    for k, array in freqs_dict.items():
+        if len(array) == max_length:
+            freqs_mean = array
+            break
+               
+    return psd_mean, freqs_mean        
+
